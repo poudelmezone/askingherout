7aa1946861bef668841ceef31117284264639f6e
MezonePoudel <145553532+poudelmezone@users.noreply.github.com>
2025-04-19 14:58:09 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const noButton = document.getElementById('noButton');
     const yesButton = document.getElementById('yesButton');
+    const txt = document.getElementById('txt');
     let clickCount = 0;
-    const texts = ['really?', 'u sure?', 'u fr?', 'for sure?', 'Okay!😔'];
+    const texts = ['really?', 'u sure?', 'u fr?', 'for sure?', 'deadass?', 'nahh 😭', 'brooo💀', 'you sure?', 'say less 😤', 'ight then…', 'whatever😒', 'lmao ok', 'omg 😭', 'woww', 'aight cool', 'u serious?', 'sheesshhh', 'ok ig😔'];
 
     noButton.addEventListener('click', (event) => {
         event.preventDefault(); // Prevent the default action to keep the button functional
-        if (clickCount < 5) {
+        if (clickCount < 18) {
             clickCount++;
-            noButton.style.transform = `scale(${1 + clickCount * 0.2})`;
+            yesButton.style.transform = `scale(${1 + clickCount * 0.2})`;
             noButton.querySelector('.no').textContent = texts[clickCount - 1];
             if (window.innerWidth > 768) {
-                yesButton.style.transform = 'translateX(-50px)'; // Move the YES button to the left
+                noButton.style.transform = 'translateX(200px)'; // Move the YES button to the left
+                txt.style.transform = 'translateY(-100px)';
             }
         }
     });
